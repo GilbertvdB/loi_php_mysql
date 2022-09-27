12bcd6bcb10fb079f88d35354d14a2b382694c2d
Gilbert van der Biezen <101508384+GilbertvdB@users.noreply.github.com>
2022-09-27 02:09:28 +0200

    index file for live site

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+Hello world!
+
+</body>
+</html>
